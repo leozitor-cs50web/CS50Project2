1ac5ecfb82a76ec54c19cd7cc8fe26de7f59fe7d
Leozitor Floro de Souza <leozitor@gmail.com>
2020-04-11 21:41:48 -0400

    criacao de canal usando sockets concluida

--- application.py
+++ application.py
@@ -1,13 +1,27 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# list of channels
+channels = ["# General"]
+
+# list of users
+users = set()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", channels=channels)
+
+
+@socketio.on("create channel")
+def createChannel(data):
+    if data in channels:
+        emit("error")
+    else:
+        channels.append(data)
+        emit("announce channel creation", data, broadcast=True)
